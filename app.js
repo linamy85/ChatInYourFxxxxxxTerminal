fb57637e98b510e3c20c2ad58947d2ff65cf71c6
hang <chatinterm@gmail.com>
2016-12-22 16:23:19 +0000

    Basic login done

--- app.js
+++ app.js
@@ -4,9 +4,14 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require('express-session');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+
+var mongoose = require('mongoose');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
 
 var app = express();
 
@@ -17,13 +22,31 @@ app.set('view engine', 'ejs');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+    secret: 'keyboard cat'
+    //resave: false,
+    //saveUninitialized: true,
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/', index);
 app.use('/users', users);
+
+
+// passport config
+var Account = require('./models/account');
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
+
+// mongoose
+mongoose.connect('mongodb://localhost/cn2016');
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,54 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
+var Account = require('../models/account');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'CN2016', user: req.user, error: req.session.error });
 });
+
+router.get('/register', function(req, res) {
+      res.render('register', {});
+});
+
+router.post('/register', function(req, res) {
+  Account.register(
+    new Account({ username : req.body.username }),
+    req.body.password, 
+    function(err, account) {
+      if (err) {
+        return res.render('register', { account : account, error: "ID existed" });
+      }
+      passport.authenticate('local')(req, res, function () {
+        req.session.save(function (err) {
+          if (err) {
+            return next(err);
+          }
+          res.redirect('/');
+        });
+      });
+    }
+  ); // Account.register
+});
+
+router.get('/login', function(req, res) {
+  res.render('login', { user : req.user  });
+});
+
+router.post('/login', passport.authenticate('local'), function(req, res) {
+  console.log(req.user ,"logs in.")
+  res.redirect('/');
+});
+
+router.get('/logout', function(req, res) {
+  req.logout();
+  res.redirect('/');
+});
+
+router.get('/ping', function(req, res){
+  res.status(200).send("pong!");
+});
+
 
 module.exports = router;
