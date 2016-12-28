58f552e4908cc0ff37c437374ef3ad65abcabe7d
Amy Lin <linamy85@gmail.com>
2016-12-29 00:02:49 +0800

    Add socket.io to sync connection

--- app.js
+++ app.js
@@ -5,15 +5,19 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var session = require('express-session');
+var debug = require('debug')('chatinyourfxxxxxxterminal:server');
+var http = require('http');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var message = require('./routes/message.js');
 
 var mongoose = require('mongoose');
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 
 var app = express();
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -36,6 +40,7 @@ app.use(passport.session());
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/message', message);
 
 
 // passport config
@@ -66,4 +71,104 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-module.exports = app;
+
+///////////// Creates HTTP server & bind with socket.io //////////
+
+var server = http.createServer(app);
+
+var io = require('socket.io').listen(server);
+
+///////////// Socket.io ///////////////
+var Message = require('./models/message');
+
+io.sockets.on('connection', function(socket) {
+  // User sends messages.
+  socket.on('chat message', function(text) {
+    console.log(socket.username, "sends", text, "to", socket.room);
+    // TODO: store to DB (need protocol like counter??)
+    var msg = new Message({
+      roomID: socket.room,
+      sender: socket.username,
+      text: text,
+      sticker: ""
+    })
+    msg.save(function(err) {
+      if (err) {
+        socket.emit("Update failed", msg);
+        return console.log("Save new message: ", err);
+      }
+      socket.broadcast.to(socket.room).emit("chat message", socket.username, text);
+    })
+  });
+  
+  socket.on('join room', function(roomid) {
+    socket.room = roomid;
+    socket.join(roomid);
+  });
+  socket.on('set name', function(name) {
+    socket.username = name;
+  })
+
+  socket.on('disconnect', function() {
+    socket.leave(socket.room)
+  })
+});
+
+
+
+////////////// Port binding /////////////
+
+var port = normalizePort(process.env.PORT || '7878');
+app.set('port', port);
+
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
+
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
+
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  var bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+function onListening() {
+  var addr = server.address();
+  var bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+  console.log('Listening on ' + bind);
+}
